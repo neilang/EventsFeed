4c4a154392d449fe6cfd2ff56902ef2fb8e8680a
neilang <neilang@gmail.com>
2011-09-12 19:43:16 +1000

    Added more validation to the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ActiveRecord::Base
   validates_presence_of :title
+  validates_presence_of :start_date
+  validates_presence_of :end_date
+  validates_presence_of :description
+  validate :end_date_comes_after_start_date
+  
+  def end_date_comes_after_start_date
+    if end_date < start_date
+      errors.add(:end_date, "must come after start date")
+    end
+  end
 end
