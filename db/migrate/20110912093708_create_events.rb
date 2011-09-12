fdc0766132d8a38c7d415f102cbe15981113f2a9
neilang <neilang@gmail.com>
2011-09-12 19:39:13 +1000

    Generated events model

---
+++ db/migrate/20110912093708_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.datetime :start_date
+      t.datetime :end_date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
